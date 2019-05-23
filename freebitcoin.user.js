f28f16fda1808fc4275a4d7ee7ed41b5a5452bb6
RyukSniper <ryuksniper2@gmail.com>
2019-05-23 10:16:06 +0200

    1.9.1.6

--- freebitcoin.user.js
+++ freebitcoin.user.js
@@ -4,7 +4,7 @@
 // @author       RyukSniper
 // @match        https://freebitco.in/*
 // @grant        unsafeWindow
-// @version 1.9.1.5
+// @version 1.9.1.6
 // @downloadURL https://raw.githubusercontent.com/RyukSniper/FreebitcoinScript/master/freebitcoin.user.js
 // @updateURL https://raw.githubusercontent.com/RyukSniper/FreebitcoinScript/master/freebitcoin.user.js
 // ==/UserScript==
@@ -77,7 +77,7 @@
                         }, 60000);
                     } else {
                         if (isNaN(timeremaning.time)) {
-                            window.alert("ready");
+                            console.log("Timer Disattivo puoi cliccare");
                             setTimeout(function() {
                                 location.reload();
                             }, 60000);
