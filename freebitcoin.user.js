3aae322c25ed72f70fcc8b66def349ece52e7c32
RyukSniper <ryuksniper2@gmail.com>
2019-06-17 12:16:59 +0200

    test bonus attivo no captcha 2°

--- freebitcoin.user.js
+++ freebitcoin.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Freebitco Auto Faucet
+// @NaNe         Freebitco Auto Faucet
 // @description  Auto Faucet Script by RyukSniper
 // @author       RyukSniper
 // @match        https://freebitco.in/*
 // @grant        unsafeWindow
-// @version 1.9.2.7
+// @version 1.9.2.9
 // @downloadURL https://raw.githubusercontent.com/RyukSniper/FreebitcoinScript/master/freebitcoin.user.js
 // @updateURL https://raw.githubusercontent.com/RyukSniper/FreebitcoinScript/master/freebitcoin.user.js
 // ==/UserScript==
@@ -20,7 +20,7 @@
         console.log("Hai " + reward.points + " punti");
         reward.captcha = parseInt($('.play_without_captcha_description .bold span').text());
         if (isNaN(reward.captcha)) {
-            console.log("Timer attivo")
+            console.log("Timer attivo o Bonus attivo")
         } else {
             console.log("Il costo senza captcha è " + reward.captcha);
         }
@@ -32,69 +32,13 @@
         console.log("Oggi è il " + datagiorno + "°" + "giorno");
         if (datagiorno > 5 && datagiorno < 7) {
             console.log("WEEK");
-            if (reward.captcha <= 24) {
-                console.log("sta per cliccare");
-                $("#play_without_captchas_button").click();
+            if (isNaN(reward.captcha) && isNaN(timeremaning.time)) {
                 $("#free_play_form_button").click();
-                    setTimeout(function() {
-                        location.reload();
-                    }, 120000);
+                setTimeout(function() {
+                    location.reload();
+                }, 120000);
             } else {
-                if (timeremaning.time < 5) {
-                    console.log("mancano " + timeremaning.time + " Minuti");
-                    console.log("mancano 5 minuti o meno");
-                    setTimeout(function() {
-                        location.reload();
-                    }, 60000);
-                } else {
-                    console.log("mancano " + timeremaning.time + " Minuti");
-                    setTimeout(function() {
-                        location.reload();
-                    }, 120000);
-                }
-            }
-        } else {
-            if (dataore <= 23 && dataore >= 9) {
-                if (dataore >= 13 && dataore < 14) {
-                    console.log("Orario di pranzo a lavoro");
-                    if (reward.captcha < 12) {
-                        console.log("sta per cliccare");
-                        $("#play_without_captchas_button").click();
-                        $("#free_play_form_button").click();
-                        setTimeout(function() {
-                            location.reload();
-                        }, 120000);
-                    } else {
-                        console.log("mancano " + timeremaning.time + " Minuti");
-                        setTimeout(function() {
-                            location.reload();
-                        }, 120000);
-                    }
-                } else {
-                    console.log("orario di lavoro");
-                    if (timeremaning.time < 5) {
-                        if (isNaN(timeremaning.time)) {
-                            console.log("Clicccare");
-                            setTimeout(function() {
-                                location.reload();
-                            }, 120000);
-                        } else {
-                            console.log("mancano " + timeremaning.time + " Minuti");
-                            console.log("mancano 5 minuti o meno");
-                            setTimeout(function() {
-                                location.reload();
-                            }, 60000);
-                        }
-                    } else {
-                        console.log("mancano " + timeremaning.time + " Minuti");
-                        setTimeout(function() {
-                            location.reload();
-                        }, 120000);
-                    }
-                }
-            } else {
-                console.log("orario di clicking");
-                if (reward.captcha < 12) {
+                if (reward.captcha <= 24) {
                     console.log("sta per cliccare");
                     $("#play_without_captchas_button").click();
                     $("#free_play_form_button").click();
@@ -102,7 +46,6 @@
                         location.reload();
                     }, 120000);
                 } else {
-                    console.log("Ancora deve cliccare");
                     if (timeremaning.time < 5) {
                         console.log("mancano " + timeremaning.time + " Minuti");
                         console.log("mancano 5 minuti o meno");
@@ -114,6 +57,91 @@
                         setTimeout(function() {
                             location.reload();
                         }, 120000);
+                    }
+                }
+            }
+        } else {
+            if (dataore <= 23 && dataore >= 9) {
+                if (dataore >= 13 && dataore < 14) {
+                    console.log("Orario di pranzo a lavoro");
+                    if (isNaN(reward.captcha) && isNaN(timeremaning.time)) {
+                        $("#free_play_form_button").click();
+                        setTimeout(function() {
+                            location.reload();
+                        }, 120000);
+                    } else {
+                        if (reward.captcha < 12) {
+                            console.log("sta per cliccare");
+                            $("#play_without_captchas_button").click();
+                            $("#free_play_form_button").click();
+                            setTimeout(function() {
+                                location.reload();
+                            }, 120000);
+                        } else {
+                            console.log("mancano " + timeremaning.time + " Minuti");
+                            setTimeout(function() {
+                                location.reload();
+                            }, 120000);
+                        }
+                    }
+                } else {
+                    console.log("orario di lavoro");
+                    if (isNaN(reward.captcha) && isNaN(timeremaning.time)) {
+                        $("#free_play_form_button").click();
+                        setTimeout(function() {
+                            location.reload();
+                        }, 120000);
+                    } else {
+                        if (timeremaning.time < 5) {
+                            if (isNaN(timeremaning.time)) {
+                                console.log("Clicccare");
+                                setTimeout(function() {
+                                    location.reload();
+                                }, 120000);
+                            } else {
+                                console.log("mancano " + timeremaning.time + " Minuti");
+                                console.log("mancano 5 minuti o meno");
+                                setTimeout(function() {
+                                    location.reload();
+                                }, 60000);
+                            }
+                        } else {
+                            console.log("mancano " + timeremaning.time + " Minuti");
+                            setTimeout(function() {
+                                location.reload();
+                            }, 120000);
+                        }
+                    }
+                }
+            } else {
+                console.log("orario di clicking");
+                if (isNaN(reward.captcha) && isNaN(timeremaning.time)) {
+                    $("#free_play_form_button").click();
+                    setTimeout(function() {
+                        location.reload();
+                    }, 120000);
+                } else {
+                    if (reward.captcha < 12) {
+                        console.log("sta per cliccare");
+                        $("#play_without_captchas_button").click();
+                        $("#free_play_form_button").click();
+                        setTimeout(function() {
+                            location.reload();
+                        }, 120000);
+                    } else {
+                        console.log("Ancora deve cliccare");
+                        if (timeremaning.time < 5) {
+                            console.log("mancano " + timeremaning.time + " Minuti");
+                            console.log("mancano 5 minuti o meno");
+                            setTimeout(function() {
+                                location.reload();
+                            }, 60000);
+                        } else {
+                            console.log("mancano " + timeremaning.time + " Minuti");
+                            setTimeout(function() {
+                                location.reload();
+                            }, 120000);
+                        }
                     }
                 }
             }
