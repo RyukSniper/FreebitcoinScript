188f8d403ee9bbaf057855fde6c5460df4ba1e2a
RyukSniper <ryuksniper2@gmail.com>
2019-06-19 11:59:06 +0200

    1.9.3

--- freebitcoin.user.js
+++ freebitcoin.user.js
@@ -4,7 +4,7 @@
 // @author       RyukSniper
 // @match        https://freebitco.in/*
 // @grant        unsafeWindow
-// @version 1.9.2.9
+// @version 1.9.3
 // @downloadURL https://raw.githubusercontent.com/RyukSniper/FreebitcoinScript/master/freebitcoin.user.js
 // @updateURL https://raw.githubusercontent.com/RyukSniper/FreebitcoinScript/master/freebitcoin.user.js
 // ==/UserScript==
