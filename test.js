c23a38a611138f09779e34af27d7eb1bc2d20bfb
RyukSniper <ryuksniper2@gmail.com>
2019-07-01 17:38:25 +0200

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
         var arrayprice = $.ajax({
             url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur"
         });
-        var euro = arrayprice[responseJSON][bitcoin][eur];
+        var euro = arrayprice["responseJSON"]["bitcoin"]["eur"];
