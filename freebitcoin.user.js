94e3cb6516011fe61f9833e888467d78d4f27371
RyukSniper <ryuksniper2@gmail.com>
2019-07-01 15:25:40 +0200

    correct response

--- freebitcoin.user.js
+++ freebitcoin.user.js
@@ -14,7 +14,7 @@
     var arrayprice = $.ajax({
         url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=eur",
     });
-    var euro = arrayprice["responseJSON"]["bitcoin"]["eur"];
+    var euro = arrayprice["responseText"]["bitcoin"]["eur"];
     var reward = {};
     var timeremaning = {}
     var balance = $("#balance").text();
