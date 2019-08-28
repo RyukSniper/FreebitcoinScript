f1cf52a4db5ed6a7929f0da6d40b5370d5aa893e
RyukSniper <ryuksniper2@gmail.com>
2019-08-28 17:24:43 +0200

    Update freebitcoin.user.js
    minor

--- freebitcoin.user.js
+++ freebitcoin.user.js
@@ -4,7 +4,7 @@
 // @author       RyukSniper
 // @match        https://freebitco.in/*
 // @grant        unsafeWindow
-// @version 2.0.1
+// @version 2.0.2
 // @downloadURL https://raw.githubusercontent.com/RyukSniper/FreebitcoinScript/master/freebitcoin.user.js
 // @updateURL https://raw.githubusercontent.com/RyukSniper/FreebitcoinScript/master/freebitcoin.user.js
 // @require          https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
@@ -26,9 +26,10 @@ var reward = {};
             reward.bonustime.min = parseInt(reward.bonustime.text.split(":")[1]);
             reward.bonustime.sec = parseInt(reward.bonustime.text.split(":")[2]);
             reward.bonustime.current = reward.bonustime.hour * 3600 + reward.bonustime.min * 60 + reward.bonustime.sec;
-        } else
+        } else{
             reward.bonustime.current = 0;
         console.log(reward.bonustime.current);
+        }
         if (reward.bonustime.current !== 0) {
             console.log(reward.bonustime.current);
         } else {
